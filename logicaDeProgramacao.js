16c8bd53ee4281df703083e4da3b2e6e77674793
KauanyBeatriz <110268844+KauanyBeatriz@users.noreply.github.com>
2024-11-24 15:35:44 -0300

    Atualização_1

--- logicaDeProgramacao.js
+++ logicaDeProgramacao.js
@@ -184,5 +184,206 @@
             nomesPokemon.shift() //-> Remove o primeiro elemento
             console.log(nomesPokemon)
             console.log ("Removeu o primeiro, sobrou: "+ nomesPokemon)
+            console.log ("")
 
+//OPERADORES
+
+    //operação matematicas na programação
+        console.log("EXEMPLO 11:")
+        //Operadores Aritméticos:
+            
+            //(+)-> Adição
+            let Adição = 5 + 3;
+            console.log ("Adição: 5 + 3 = "+ Adição)
+            console.log ("")
+
+            //(-)-> Subtração
+            let Subtração = 8 - 2;
+            console.log ("Subtração: 8 - 2  = "+ Subtração)
+            console.log ("")
+
+            //(*)-> Multiplicação
+            let Multiplicação = 4 * 6;
+            console.log ("Multiplicação: 4 * 6 = "+ Multiplicação)
+            console.log ("")
+
+            //(/)-> Divisão
+            let Divisão = 10 / 2;
+            console.log ("Divisão: 10 / 2 = "+ Divisão)
+            console.log ("")
+
+            //(%) Módulo (resto da divisão)
+            let Modulo = 10 % 3;
+            console.log ("Modulo: 10 % 3 = "+ Modulo + " esse apresenta o resto da divisão")
+            console.log ("")
+
+    // É possivel fazer uma operação diretamente pela variavel
+    // Na programação não se uma virgula.
+
+        console.log ("EXEMPLO 12: ")
+            let precoProduto = 100.99
+            let valorComTaxa = precoProduto * 2
+            console.log (valorComTaxa)
+            console.log ("")
+        
+        console.log ("EXEMPLO 13: ")
+            let contador = 1
+            contador++ //soma mais 1
+            console.log ("Incrementando, numero 1 ++ = "+ contador)
+
+            console.log ("EXEMPLO 13: ")
+            let contador2 = 1
+            contador-- //subtrai 1
+            console.log ("Decrementando, numero 1-- = "+ contador2)
+            console.log ("")
+
+        //Operadores de atribuição
+
+        console.log ("EXEMPLO 14 - Operadores de atribuição: ")
+        console.log ("")
+            
+            //(=)-> Atribuição
+            let x = 5;
+            console.log (x)
+
+            //(+=)-> Adição e atribuição
+            let num1 = 10; 
+            num1 += 2; //-> mesmo que "num1 = num1 + 2"
+            console.log (num1)
+
+            //(-=)-> Subtração e atribuição
+            let num2 = 10; 
+            num2 -= 3;
+            console.log (num2)
+
+            //(*=)-> Multiplicação e atribuição
+            let num3 = 5; 
+            num3 *= 4;
+            console.log (num3)
+
+            //(/=)-> Divisão e atribuição
+            let num4 = 10; 
+            num4 /= 2;
+            console.log (num4)
+
+            //(%=)-> Módulo e atribuição
+            let num5 = 10; 
+            num5 %= 3;
+            console.log (num5)
+            console.log("")
+            
+        console.log ("EXEMPLO 15 - Expressões: ")
+        console.log ("")
+
+            let resul = 2 * 5 + 5 - 10
+            console.log ("2 * 5 + 5 - 10 = " + resul)
+            console.log ("")
+
+            resul = 2 * ((5 + 5) - 10)
+            console.log ("2 * ((5 + 5) - 10) = " + resul + " -> Forma correta seguindo a ordem das expressões")
+            console.log ("")
+
+    //Operadores Relacionais/comparação: 
+        console.log ("EXEMPLO 16 - Relacionais: ")
+        console.log ("")  
+        
+            console.log("Igual/Comparação")
+            console.log("")
+            
+            let marca = "Apple"
+            let resultado = marca === "Samsung"
+
+            console.log(resultado)
+            // = é atribuição
+            // == é para comparar o valor
+            // === é para comparar o valor e o formato do conteúdo
+            // !== é diferente ?
+            
+            console.log("Diferente")
+              let isNotEqual = "Maça"
+              console.log (isNotEqual != "Maça")
+              console.log (isNotEqual !== 2)
+            
+            console.log("EX: Menor ou igual a")
+              let idadeDeCorte = 50
+              let idadeUsuario = 20
+              
+              let resultadoEhTerceiraIdade = idadeDeCorte <= idadeUsuario
+              console.log("50 é maior 20: " + resultadoEhTerceiraIdade)
+              console.log ("")
+
+            //(>)-> Maior que
+            //  let isGreater = (x > y);
+            //(<)-> Menor que
+            //  let isLess = (x < y);
+            //(>=)-> Maior ou igual a
+            //  let isGreaterOrEqual = (x >= y);
+            //(<=)-> Menor ou igual a
+            //  let isLessOrEqual = (x <= y);
+
+    //Operadores Lógicos:
+
+        console.log ("EXEMPLO 16 - Operadores logicos: ")
+
+            //(&&)-> AND lógico
+            //if (condition1 && condition2)
+
+              let idade = 18
+              let vistoVerificado = false
+              resultado =  (idade >= 18) && (vistoVerificado === true) 
+              console.log("É maior de idade e o visto está verificado?" + resultado)
+              console.log ("")
+
+            //(||)-> OR lógico
+            //if (condition1 || condition2)
+              
+              let clima = "chuva"
+              let item = "guarda chuva"
+              let podeSair = (tempo !== "chuva") || (item === "guarda chuva")
+              console.log("Não está chovendo ou está com o guarda chuva? " + podeSair)
+              console.log ("")
+
+            //(!)-> NOT lógico
+            //if (!condition)
+
+              let tempo = "chuva"
+              let horario = 8
+              let resultado1 = !((tempo !== "chuva") && (horario > 6)) //-> o ! no inicio muda a polaridade, !! tambem muda
+              console.log(resultado)
+              console.log("")
+
+//CONDICIONAIS
+
+    //if
+        console.log ("EXEMPLO 17 - Condicionais: ")
+
+            //If - else - Exemplo: Só vai comprar ovos, se tiver leite. senão compra lasanha.
+
+              let possuiOvos = true
+              let itensComprados = ""
+
+              if(possuiOvos){
+                itensComprados = "Leite"
+              }
+              else{
+                console.log("passou na sessão de congelados")
+                itensComprados = "Lasanha congelada"
+              }
+
+              console.log("item comprado: " + itensComprados)
+
+            //If - else - else if ->Exemplo: nivel de fome.
+
+              let nivelDeFome =  "1"
+
+              if(nivelDeFome === 1){
+                console.log("pouca fome")
+              }else if(nivelDeFome ===2){
+                console.log("muita fome")
+              }else{
+                console.log("Você comeria mais que o pica pau")
+              }
+
+
+            
 
