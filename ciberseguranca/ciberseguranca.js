a157e4e9898476a866bb7855e85ded14e8810fe8
KauanyBeatriz <110268844+KauanyBeatriz@users.noreply.github.com>
2024-12-04 22:06:57 -0300

    Estudando cibersegurança

---
+++ ciberseguranca/ciberseguranca.js
@@ -0,0 +1,99 @@
+// O QUE É CIBERSEGURANÇA?
+    
+    //É um conjunto de ações e técnicas voltadas para proteger sistemas, programas, redes e equipamentos contra invasões.
+    
+    // Foco é garantir que dados valiosos não vazem ou sejam violados.
+
+    //TIPOS:
+
+        // 1-> Segurança operacional: define quais pessoas tem acesso ou niveis de acesso que um determinado funcionario pode obter.
+        // 2-> Segurança de rede: proteger a rede contra acessos indevidos e ataques com DoS (Denial of Service -> carregar até derrubar o servidor), a pontas de rede físicas também é necessaria.
+        // 3-> Segurança de aplicativos: proteção contra brechas de programação, gafes no desenvolvimento.
+        // 4-> Recuperação de desastres: Práticas de organização em caso de desastres, para a recuperação mais rápida e com menos danos possíveis.
+        // 5-> Educação do usuário final: Encontrar e corrigir comportamentos de risco dos usuários que podem expor dados sensíveis.
+
+    //PILARES:
+
+        // Identificar: identificar os quais são os dados possiveis, quem poderia atacar? como poderia atacar?
+        // Proteger: politicas de cibersegurança.
+        // Detectar: procurar brechas, monitorar, detectar padrões.
+        // Resposta: tempo de resposta, metodos.
+        // Recover: contra ataque.
+
+// O QUE É HACKING?
+    
+    // Significa comprometer sistemas, contas pessoais. Porem não necessariamente mal intencionado, depende das intenções do individio
+    
+    // Cracker -> Hacker invade uma rede ou sistema de forma maliciosa.
+
+    // TIPOS:
+
+        // Hackers black hat (chapéu preto)-> fins malisiosos, vazamentos, extorsões.
+        // Hackers white hat (chapéu branco)-> detecta ameaças / vunerabilidades e protege sistemas, de forma concentida de acordo com contrato.
+        // Hackers grey hat (intermediarios)-> Meio termo
+    
+// PRINCIPAIS TIPOS DE AMEAÇAS
+
+    // A ameaças não é apenas direcionadas ao sistema, mas tambem contra pessoas.
+
+    // FONTES DE ATAQUES:
+
+        // NATION-STATES: motivação geopolitica.
+        // CYBERCRIMINALS: motivação lucro.
+        // HACKTIVISTS: motivação ideologia.
+        // TERRORIST GROUPS: motivação ideologia violenta.
+        // THRILL-SEEKERS: motivação satistação.
+        // INSIDER THREATS: motivação discontent.
+
+    // TIPOS DE ATAQUES:
+
+        // Phishing-> e-mails com script malicioso.
+        // Spoofing de DNS-> comprometimento de servidor, direcionamento para o servidor pessoal do hacker.
+        // Roubo de cookies-> tem cookies que trafegam com dados, permitindo que o hacker se passe por você.
+        // XSS-> Injeção de script em campos vazios para acesso aos dados.
+        // SQL injection-> Injeção de comandos sql em campos vazios para acesso aos banco de dados
+        // Malware->
+        // Web-based attacks->
+        // Spam->
+        // DOos-> carregar até derrubar o servidor
+
+//BOAS PRATICAS EM CIBERSEGURANÇA
+
+    // CATEGORIA:
+
+        // Conscientização de pessoas
+            // Reduzir o nivel de negligências dos funcionarios
+            // Informar os funcionarios sobre técnicas comuns de phishing
+
+        // Controle de acesso
+            // Proteção o acesso de dispositivos remotos
+            // Manipulação de senhas com segurança
+            // Usar o proncipio do privilegio minimo
+        
+        // Proteção de dados confidenciais
+            // Atenção aos dados privilegiados
+            // Monitoramento de acesso de terceiros
+            // Manter backup de dados confidenciais
+        
+        // Segurança robusta e proteção de rede
+            // Politicas hierarquicas de segurança
+            // Proteger a rede corporativa
+            // Auditorias regulares da segurança
+        
+        // Grenciamento de identidade
+            // Biometria de segurança
+            // Autenticação multifator
+        
+// BLUETEAM VS REDTEAM
+
+    // Metodologia para melhor o sistema identificando e verificando o sistema ao mesmo tempo que defende e corrige os erros. 
+
+    //RED TEAM -> atacar e identificar o sistema para explorar as vunerabilidades.
+        //simulação de ataques
+    
+    //BLUE TEAM -> neutralizar e corrigir as vunerabilidades.
+        // Bloquear os ataques
+    
+    //PURPLE TEAM -> Meio termo.
+
+    //Certificação: CEH e ECSA
